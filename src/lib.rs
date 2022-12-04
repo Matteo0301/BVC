cbd310b54b6647534456e9ae80cc961d6162de5b
Matteo0301 <matteomariotti0301@gmail.com>
2022-12-04 19:26:42 +0100

    Constructors

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,14 @@ pub mod bvc {
 
     use unitn_market_2022::{
         event::{event::Event, notifiable::Notifiable},
-        good::{good::Good, good_kind::GoodKind},
+        good::{consts::STARTING_CAPITAL, good::Good, good_kind::GoodKind},
         market::{
             good_label::GoodLabel, BuyError, LockBuyError, Market, MarketGetterError, SellError,
         },
     };
+
+    use rand::thread_rng;
+    use rand::Rng;
 
     const NAME: &'static str = "BVC";
 
@@ -28,18 +31,51 @@ pub mod bvc {
         where
             Self: Sized,
         {
+            let mut max = STARTING_CAPITAL;
+            let (mut eur, mut yen, mut usd, mut yuan): (f32, f32, f32, f32) = (0.0, 0.0, 0.0, 0.0);
+            let mut rng = thread_rng();
+            eur = rng.gen_range(0.0, max);
+            max -= eur;
+            yen = rng.gen_range(0.0, max);
+            max -= yen;
+            usd = rng.gen_range(0.0, max);
+            max -= usd;
+            yuan = rng.gen_range(0.0, max);
+            max -= yuan;
+            Self::new_with_quantities(eur, yen, usd, yuan)
         }
+
         fn new_with_quantities(eur: f32, yen: f32, usd: f32, yuan: f32) -> Rc<RefCell<dyn Market>>
         where
             Self: Sized,
         {
+            let m: BVC_market = BVC_market {
+                eur: Good {
+                    kind: GoodKind::EUR,
+                    quantity: eur,
+                },
+                yen: Good {
+                    kind: GoodKind::YEN,
+                    quantity: yen,
+                },
+                usd: Good {
+                    kind: GoodKind::USD,
+                    quantity: usd,
+                },
+                yuan: Good {
+                    kind: GoodKind::YUAN,
+                    quantity: yuan,
+                },
+            };
+            Rc::new(RefCell::new(m))
         }
         fn new_file(path: &str) -> Rc<RefCell<dyn Market>>
         where
             Self: Sized,
         {
-            Self::new_random();
+            Self::new_random()
         }
+
         fn get_name(&self) -> &'static str {
             NAME
         }
