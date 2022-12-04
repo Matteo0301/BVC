85b33c1e899f81a881ca0402be8e2838c881d7c4
Matteo0301 <matteomariotti0301@gmail.com>
2022-12-04 18:37:13 +0100

    Base structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,72 @@
-#![allow(non_snake_case)]
+pub mod bvc {
+    use std::rc::Rc;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+    use unitn_market_2022::{
+        event::{event::Event, notifiable::Notifiable},
+        good::good::Good,
+        market::Market,
+    };
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    const NAME: &'static str = "BVC";
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub struct BVC_market {
+        eur: Good,
+        usd: Good,
+        yen: Good,
+        yuan: Good,
+    }
+
+    impl Notifiable for BVC_market {
+        fn add_subscriber(&mut self, subscriber: Box<dyn Notifiable>) {}
+        fn on_event(&mut self, event: Event) {}
+    }
+
+    impl Market for BVC_market {
+        fn new_random() -> Rc<RefCell<dyn Market>>
+        where
+            Self: Sized,
+        {
+        }
+        fn new_with_quantities(eur: f32, yen: f32, usd: f32, yuan: f32) -> Rc<RefCell<dyn Market>>
+        where
+            Self: Sized,
+        {
+        }
+        fn new_file(path: &str) -> Rc<RefCell<dyn Market>>
+        where
+            Self: Sized,
+        {
+        }
+        fn get_name(&self) -> &'static str {}
+
+        fn get_budget(&self) -> f32 {}
+
+        fn get_buy_price(&self, kind: GoodKind, quantity: f32) -> Result<f32, MarketGetterError> {}
+
+        fn get_sell_price(&self, kind: GoodKind, quantity: f32) -> Result<f32, MarketGetterError> {}
+
+        fn get_goods(&self) -> Vec<GoodLabel> {}
+
+        fn lock_buy(
+            &mut self,
+            kind_to_buy: GoodKind,
+            quantity_to_buy: f32,
+            bid: f32,
+            trader_name: String,
+        ) -> Result<String, LockBuyError> {
+        }
+
+        fn buy(&mut self, token: String, cash: &mut Good) -> Result<Good, BuyError> {}
+
+        fn lock_sell(
+            &mut self,
+            kind_to_sell: GoodKind,
+            quantity_to_sell: f32,
+            offer: f32,
+            trader_name: String,
+        ) -> Result<String, LockSellError> {
+        }
+
+        fn sell(&mut self, token: String, good: &mut Good) -> Result<Good, SellError> {}
     }
 }
